bcc1912fbfd8d36bffde64feb9ba9fa21abccad7
Brandon Mohammed <mohammedbrandon204@gmail.com>
2024-07-11 16:17:22 -0800

    should not affect navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,19 +16,19 @@ module.exports = {
         },    
     },
     screens: {
-      '2xl': {'max': '1535px'},
+      'max-2xl': {'max': '1535px'},
       // => @media (max-width: 1535px) { ... }
 
-      'xl': {'max': '1279px'},
+      'max-xl': {'max': '1279px'},
       // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      'max-lg': {'max': '1023px'},
       // => @media (max-width: 1023px) { ... }
 
-      'md': {'max': '767px'},
+      'max-md': {'max': '767px'},
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      'max-sm': {'max': '639px'},
       // => @media (max-width: 639px) { ... }
     }
   },
